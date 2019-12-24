e71604d7a6e6bc30cf96312d57db4deed4b70742
김선영 <tjsgur35@betree.co.kr>
2019-12-24 13:02:05 +0900

    thumb

--- src/components/works/Thumb.js
+++ src/components/works/Thumb.js
@@ -4,7 +4,9 @@ const Thumb = ({data}) => {
     return (
         <>
             {data.contents.map((v, idx) =>
-                <div className="image" key={idx} style={{backgroundImage:`url(${v.image})`}} />
+                <div className="image" key={idx} style={{backgroundImage:`url(${v.image})`}}>
+                    <img src={v.image} alt={v.title} />
+                </div>
             )}
         </>
     );
